a6831c11ff8a32bbbd50a4b3ec7fecfb115b57aa
yk <ykk0214@163.com>
2018-08-30 20:07:37 +0800

    增加简历2

---
+++ job/index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+    <head>
+        <meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+        <meta name="format-detection" content="email=no"/>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="renderer" content="webkit">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-title" content="Amaze UI"/>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
+    	<title>[简历]严凯</title>
+    	<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="assets/css/typo.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/index.css">
+        <script>
+            function loading(){
+                document.getElementsByClassName('avatar')[0].style.display = 'block';
+                document.getElementsByClassName('loading')[0].style.display = 'none';
+            }
+        </script>
+    </head>
+    <body>
+
+        <header class="header"></header>
+        
+        <article class="container">
+            <section class="side">
+                <!-- 个人肖像 -->
+                <section class="me">
+                    <section class="portrait">
+                        <div class="loading">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <!-- 头像照片 -->
+                        <img class="avatar" src="assets/images/avatar.jpg" onload="loading()">
+                    </section>
+
+                    <h1 class="name">Kyle</h1>
+                    <h4 class="info-job">PHP开发工程师 / 广州</h4>
+
+                </section>
+
+                <!-- 基本信息 -->
+                <section class="profile info-unit">
+                    <h2><i class="fa fa-user" aria-hidden="true"></i>基本信息</h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            <label>个人信息</label>
+                            <span>Kyle / 男 / 24岁</span>
+                        </li>
+                        <li>
+                            <label>英语水平</label>
+                            <span>CET-4</span>
+                        </li>
+                    </ul>
+                </section>
+
+                <!-- 联系方式 -->
+                <section class="contact info-unit">
+                    <h2><i class="fa fa-phone" aria-hidden="true"></i>联系方式</h2>
+                    <hr/>
+                    <ul>
+                        <li><label>手机</label><a href="#">188-1925-3867</a></li>
+                        <li><label>邮箱</label><a href="##">3198556251@qq.com</a></li>
+                        <li><label>个人博客网站</label><a href="http://a.yklkp.top" target="_blank">a.yklkp.top/</a></li>
+                        <li><label>个人音乐网站</label><a href="http://www.ykxkun.cn" target="_blank">www.ykxkun.cn/</a></li>
+                        <li><label>Github</label><a href="https://github.com/yklkp" target="_blank">github.com/yklkp</a></li>
+                    </ul>
+                </section>
+
+                <!-- 技能点 -->
+                <section class="skill info-unit">
+                    <h2><i class="fa fa-code" aria-hidden="true"></i>技能点</h2>
+                    <hr/>
+                    <ul>
+                        <li><label>PHP</label><progress value="90" max="100"></progress></li>
+                        <li><label>HTML</label><progress value="80" max="100"></progress></li>
+                        <li><label>JavaScript</label><progress value="80" max="100"></progress></li>
+                        <li><label>css</label><progress value="80" max="100"></progress></li>
+                        <li><label>小程序</label><progress value="70" max="100"></progress></li>
+                        <li><label>PS</label><progress value="60" max="100"></progress></li>
+                    </ul>
+                </section>
+
+                <!-- 技术栈 -->
+                <div class="stack info-unit">
+                    <h2><i class="fa fa-code" aria-hidden="true"></i>框架</h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            <label>ThinkPHP5</label>
+                            <span>熟练使用，有相应的两个项目</span>
+                        </li>
+                        <li>
+                            <label>Laravel</label>
+                            <span>熟练使用，有一个大型企业级IBMS系统</span>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="main">
+
+                <!-- 教育经历 -->
+                <section class="edu info-unit">
+                    <h2><i class="fa fa-graduation-cap" aria-hidden="true"></i>教育经历</h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            <h3><span>江西理工大学应用科学学院 - 电子信息专业（本科）</span><time>2014.9-2018.7</time></h3>
+                            <p></p>
+                        </li>
+                        <li>
+                            <h3><span>在校经历</span></h3>
+                            <p>课程：C语言设计，数据结构，计算机网络等</p>
+                            <p>课设：网页网站搭建，嵌入式系统模拟，计算机网络课程设计等</p>
+                            <p>社团：读书协会会员，主持人部会员</p>
+                        </li>
+                    </ul>
+                </section>
+
+                <!-- 工作经历 -->
+                <section class="work info-unit">
+                    <h2><i class="fa fa-shopping-bag" aria-hidden="true"></i>培训经历+工作经历</h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            <h3>
+                                <span>[经历1]广州环朝商贸有限公司－PHP开发工程师（实习）</span>
+                                <time>2018.1 至 今</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>深度参与IBMS项目公司业务erp管理系统开发工作，独立承担并完成基础信息模块的设计与开发，对外报价单等多表关系的复杂模块开发，主要实现大部分的基础页面模块和核心所需的数据逻辑，部分用户体验度的设计修改。主要项目采用<mark>Laravel框架</mark>，技术使用<mark>pjax+jQuery+mysql</mark>等技术进行开发。</li>
+                                <li>配合UI和后端，根据产品需求提供H5页面嵌入到后台模板，要求<mark>移动端显示正常</mark>。</li>
+                                <li>主要参与XXXXXXX的静态页面开发工作，要求<mark>在支付宝环境下完全兼容</mark>。</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>[经历2]XX－前端开发工程师（实习）</span>
+                                <time>20XX.10-20XX.7</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>深度参与公司主线产品「XXXX」的前端开发工作，完成帖子快捷回复、<mark>全站图片懒加载</mark>、活动banner、帖子管理（使用Yii框架）等功能。项目采用技术栈phpWind+NAMP。<a href="http://itsay.tech/162/" target="_blank"><i class="fa fa-link" aria-hidden="true"></i>博客文章</a>。</li>
+                                <li>主要参与公司产品「XXXX」的前端开发工作，实现接入微博、微信、QQ等<mark>第三方账号登录</mark>等功能。项目采用技术栈WordPress+NAMP。</li>
+                                <li>活动页面的开发（七夕活动、抽奖活动以及承接外包页面）。</li>
+                                <li>论坛<mark>图片增量备份</mark>（CentOS+vsftpd+crontab）<a href="#" target="_blank"><i class="fa fa-link" aria-hidden="true"></i>博客文章</a>。</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </section>
+
+                <!-- 项目经验 -->
+                <section class="project info-unit">
+                    <h2><i class="fa fa-terminal" aria-hidden="true"></i>个人项目</h2>
+                    <hr/>
+                    <ul>
+                        <li>
+                            <h3>
+                                <span>[项目1]医学科学数据管理与共享平台</span>
+                                <span class="link"><a href="#" target="_blank">Demo</a></span>
+                                <time>201X.X-201X.X</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>技术栈：ThinkPHP+MongoDB+Axure</li>
+                                <li>
+                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                    [目标]实现多类型多来源医学科学数据的提交、管理和共享<br/>
+                                    <i class="fa fa-users" aria-hidden="true"></i>
+                                    [团队]同 2 位同专业同学一起<br/>
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    [贡献]完成从<mark>“调研-设计-实现-文档”</mark>等工作，主要负责系统原型、功能框架及数据提交流程、元数据及源数据的管理与共享方案的设计以及系统开发等工作<br/>
+                                    <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                                    [效果]作品最终取得第三届共享杯国家级竞赛“一等奖” （2/2000）</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>[项目2]肿瘤流行病数据可视化</span>
+                                <span class="link"><a href="#" target="_blank">Demo</a></span>
+                                <time>201X.X-201X.X</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>技术栈：HTML 5+D3.js+ECharts+MySQL</li>
+                                <li>
+                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                    [目标]实现常见肿瘤流行病数据多维度可视化展示、数据透视及分析<br/>
+                                    <i class="fa fa-users" aria-hidden="true"></i>
+                                    [团队]与 1 位同学<br/>
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    [贡献]分析项目需求，清洗并整理相关数据(扩展第三方知识组织系统和 Google trends 数据)，并用 <mark>D3.js</mark> 和 <mark>ECharts</mark> 进行图形化展示以及实现简易自动分析 功能<br/>
+                                    <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                                    [效果]作品取得第二届共享杯国家级竞赛“特等奖”(1/1500)
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>[项目3]肿瘤流行病数据可视化</span>
+                                <span class="link"><a href="#" target="_blank">Demo</a></span>
+                                <time>201X.X-201X.X</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>技术栈：HTML 5+D3.js+ECharts+MySQL</li>
+                                <li>
+                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                    [目标]实现常见肿瘤流行病数据多维度可视化展示、数据透视及分析<br/>
+                                    <i class="fa fa-users" aria-hidden="true"></i>
+                                    [团队]与 1 位同学<br/>
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    [贡献]分析项目需求，清洗并整理相关数据(扩展第三方知识组织系统和 Google trends 数据)，并用 <mark>D3.js</mark> 和 <mark>ECharts</mark> 进行图形化展示以及实现简易自动分析功能<br/>
+                                    <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                                    [效果]作品取得第二届共享杯国家级竞赛“特等奖”(1/1500)
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>
+                                <span>[项目4]肿瘤流行病数据可视化</span>
+                                <span class="link"><a href="#" target="_blank">Demo</a></span>
+                                <time>201X.X-201X.X</time>
+                            </h3>
+                            <ul class="info-content">
+                                <li>技术栈：HTML 5+D3.js+ECharts+MySQL</li>
+                                <li>
+                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                    [目标]实现常见肿瘤流行病数据多维度可视化展示、数据透视及分析<br/>
+                                    <i class="fa fa-users" aria-hidden="true"></i>
+                                    [团队]与 1 位<br/>
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                    [贡献]分析项目需求，清洗并整理相关数据(扩展第三方知识组织系统和 Google trends 数据)，并用 <mark>D3.js</mark> 和 <mark>ECharts</mark> 进行图形化展示以及实现简易自动分析 功能<br/>
+                                    <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                                    [效果]作品取得第二届共享杯国家级竞赛“特等奖”(1/1500)
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </section>
+
+                <!-- 自我评价 -->
+                <section class="work info-unit">
+                    <h2><i class="fa fa-pencil" aria-hidden="true"></i>自我评价/期望</h2>
+                    <hr/>
+                    <p>[此处如果有一些能够量化的、且比较个性的指标会有加分，比如喜爱跑步坚持夜跑200小时或者200km等]<span class="mark-txt">“多静多思考，反省不张扬”</span>是我给自己总结的“十字箴言”，鞭策自己做人既不能以己度人，也不以人观己，要脚踏实地做事，坚持自己的梦想和本心。</p>
+                </section>
+            </section>
+        </article>
+
+        
+
+        <footer class="footer">
+            <p>© 2018 凯哥.文档最后更新时间为<time>2018年5月3日</time>.</p>
+        </footer>
+
+        <!-- 侧栏 -->
+        <aside>
+            <ul>
+                <li><a href="https://job.ykxkun.cn/blog" target="_blank">了解更多</a></li>
+                <li><a href="https://job.ykxkun.cn/blog" target="_blank">Blog</a></li>
+            </ul>
+        </aside>
+
+        <script src="./assets/js/index.js"></script>
+    </body>
+</html>
